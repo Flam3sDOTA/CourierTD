01f63978409958f52daa301fb005c2d7b0970163
Flam3sDOTA <30335579+Flam3sDOTA@users.noreply.github.com>
2019-06-13 01:25:08 +0300

    latest
    latest

--- content/dota_addons/liquidtd/panorama/scripts/custom_game/resources.js
+++ content/dota_addons/liquidtd/panorama/scripts/custom_game/resources.js
@@ -17,10 +17,181 @@ function OnExchangeButtonPressed() {
     }
     else{
         Game.EmitSound("ui_select_blue");
-		TangoAmount = TangoAmount+1;
-		$('#LumberText').text = TangoAmount;
+		var playerID = Players.GetLocalPlayer();
+		var data = CustomNetTables.GetTableValue("tango", playerID);
+		if (data && data.value) {
+			$('#LumberText').text = data.value;
+		}
     }
 }
+
+function DamagePressed20Classic() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+
+function DamagePressed50Classic() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+
+function DamagePressed100Classic() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+
+function ManaCost20Classic() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function ManaCost30Classic() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function ManaCost50Classic() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD1Classic() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD2Classic() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD3Classic() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function DamagePressed20Utility() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+
+function DamagePressed50Utility() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+
+function DamagePressed100Utility() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function ManaCost20Utility() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function ManaCost30Utility() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function ManaCost50Utility() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD1Utility() {
+	if(Tango < 50){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD2Utility() {
+	if(Tango < 250){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
+function CD3Utility() {
+	if(Tango < 750){
+        Game.EmitSound("versus_screen.towers_nopass");
+    }
+    else{
+	Game.EmitSound("ui_select_blue");
+	}
+}
+
 
 (function () {
   UpdateGold();
