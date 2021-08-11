2babcfd3d63798f9083b2fe22896ff298b119859
Flam3s <iletisimcarl@gmail.com>
2021-08-11 10:27:02 +0300

    fix

--- content/dota_addons/liquidtd/panorama/scripts/custom_game/mainhud.js
+++ content/dota_addons/liquidtd/panorama/scripts/custom_game/mainhud.js
@@ -169,10 +169,10 @@ function SchedulePopupClose()
 }
 
 function SecondsToClock(seconds) {
-  const sec_num = parseInt(seconds, 10);
-  const hours   = Math.floor(sec_num / 3600);
-  const minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  const seconds = sec_num - (hours * 3600) - (minutes * 60);
+  var sec_num = parseInt(seconds, 10);
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
   if (hours   < 10) {hours   = "0"+hours;}
   if (minutes < 10) {minutes = "0"+minutes;}
